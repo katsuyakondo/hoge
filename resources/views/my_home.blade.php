eb18984b7fd0a1c1290e735265785b6362382828
katsuyakondo <kondo318@gmail.com>
2024-03-28 03:50:17 +0900

    php内のjsコード削除

--- resources/views/my_home.blade.php
+++ resources/views/my_home.blade.php
@@ -23,21 +23,3 @@
     </div>
 </body>
 
-<script>
-    async function sendToAI() {
-    const input = document.getElementById('auto-focus-field').value;
-    // URLをLaravelのルーティングに合わせて'/chat'に変更
-    const response = await fetch('/chat', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', // ヘッダーをJSONに変更
-        },
-        body: JSON.stringify({
-            userInput: input // ボディの形式もJSONに合わせて修正
-        })
-    });
-    const data = await response.json();
-    document.getElementById('aiResponse').innerText = data.choices[0].message.content;
-}
-
-</script>
